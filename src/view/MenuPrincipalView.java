cc1429bd14642807b53d66e1cf754a629961e27a
vitorsneves <viniciosneves10@gmail.com>
2019-06-06 12:06:46 -0300

    Novo menu

---
+++ src/view/MenuPrincipalView.java
@@ -0,0 +1,108 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MenuPrincipalView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipalView frame = new MenuPrincipalView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuPrincipalView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(50, 30, 1280, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("MENU");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 60));
+		lblNewLabel.setBounds(544, 59, 189, 109);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Nova compra");
+		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnNewButton.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/dinheiro.png")));
+		btnNewButton.setBounds(156, 202, 415, 82);
+		contentPane.add(btnNewButton);
+		
+		JButton btnAdicionarCliente = new JButton("Adicionar cliente");
+		btnAdicionarCliente.setHorizontalAlignment(SwingConstants.LEFT);
+		btnAdicionarCliente.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnAdicionarCliente.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/cliente.png")));
+		btnAdicionarCliente.setBounds(156, 377, 415, 82);
+		contentPane.add(btnAdicionarCliente);
+		btnAdicionarCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CriarClienteView frame = new CriarClienteView();
+				frame.setVisible(true);
+			}
+		});
+		JButton btnNewButton_1 = new JButton("Procurar em estoque");
+		btnNewButton_1.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton_1.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnNewButton_1.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/estoque.png")));
+		btnNewButton_1.setBounds(156, 552, 415, 82);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Estat\u00EDsticas");
+		btnNewButton_2.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton_2.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnNewButton_2.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/estatisticas.png")));
+		btnNewButton_2.setBounds(705, 202, 415, 82);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Op\u00E7\u00F5es adicionais");
+		btnNewButton_3.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton_3.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnNewButton_3.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/chave.png")));
+		btnNewButton_3.setBounds(705, 377, 415, 82);
+		contentPane.add(btnNewButton_3);
+		
+		JLabel fundoEscuro = new JLabel("");
+		fundoEscuro.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/fundoEscuro.png")));
+		fundoEscuro.setBounds(32, 38, 1158, 693);
+		contentPane.add(fundoEscuro);
+		
+		JLabel fundo = new JLabel("");
+		fundo.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/fundoMenu.jpg")));
+		fundo.setBounds(0, 0, 1920, 1080);
+		contentPane.add(fundo);
+		
+		setResizable(false);
+	}
+}
---
+++ src/view/OpcoesAdicionaisView.java
@@ -0,0 +1,118 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class OpcoesAdicionaisView extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnMudarSenha;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OpcoesAdicionaisView frame = new OpcoesAdicionaisView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OpcoesAdicionaisView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(50, 30, 1280, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		//título "menu" no topo da página
+		JLabel menu = new JLabel("MENU");
+		menu.setForeground(Color.WHITE);
+		menu.setFont(new Font("Arial", Font.BOLD, 60));
+		menu.setBounds(544, 59, 189, 109);
+		contentPane.add(menu);
+		
+		//botão "gerentes"
+		JButton btnGerentes = new JButton("Gerentes");
+		btnGerentes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnGerentes.setHorizontalAlignment(SwingConstants.LEFT);
+		btnGerentes.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnGerentes.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/gerente.png")));
+		btnGerentes.setBounds(156, 202, 415, 82);
+		contentPane.add(btnGerentes);
+		
+		//botão funcionários
+		JButton btnFuncionarios = new JButton("Funcion\u00E1rios");
+		btnFuncionarios.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnFuncionarios.setHorizontalAlignment(SwingConstants.LEFT);
+		btnFuncionarios.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnFuncionarios.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/funcionarios.png")));
+		btnFuncionarios.setBounds(156, 401, 415, 82);
+		contentPane.add(btnFuncionarios);
+		
+		//botão estoque
+		JButton btnEstoque = new JButton("Estoque");
+		btnEstoque.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});		
+		btnEstoque.setHorizontalAlignment(SwingConstants.LEFT);
+		btnEstoque.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnEstoque.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/estoque02.png")));
+		btnEstoque.setBounds(705, 202, 415, 82);
+		contentPane.add(btnEstoque);
+		
+		//botão alterar senha
+		btnMudarSenha = new JButton("Alterar senha");
+		btnMudarSenha.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnMudarSenha.setHorizontalAlignment(SwingConstants.LEFT);
+		btnMudarSenha.setFont(new Font("Arial", Font.PLAIN, 30));
+		btnMudarSenha.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/adminSenha.png")));
+		btnMudarSenha.setBounds(705, 401, 415, 82);
+		contentPane.add(btnMudarSenha);
+		
+		JLabel fundoEscuro = new JLabel("");
+		fundoEscuro.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/fundoEscuro.png")));
+		fundoEscuro.setBounds(32, 38, 1158, 595);
+		contentPane.add(fundoEscuro);
+		
+		JLabel fundo = new JLabel("");
+		fundo.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/fundoMenu.jpg")));
+		fundo.setBounds(0, 0, 1920, 1080);
+		contentPane.add(fundo);
+		
+		setResizable(false);
+	}
+
+}
