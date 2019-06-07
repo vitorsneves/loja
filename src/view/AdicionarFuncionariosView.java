52543ac3ad0815762710fbecb3d6416d0de47d2c
Hugo <gvgabrielvictor09@gmail.com>
2019-06-07 16:36:39 -0300

    View  add funcionario

--- src/view/AdicionarFuncionariosView.java
+++ src/view/AdicionarFuncionariosView.java
@@ -12,9 +12,11 @@ import controller.LoginController;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class AdicionarFuncionariosView extends JFrame {
 
@@ -44,7 +46,7 @@ public class AdicionarFuncionariosView extends JFrame {
 	 */
 	public AdicionarFuncionariosView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(500, 50, 411, 645);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,12 +54,17 @@ public class AdicionarFuncionariosView extends JFrame {
 		
 		funcionariosControlador = new AdicionarFuncionariosController(this);
 		
-		JLabel lblNewLabel = new JLabel("Nome do novo funcion\u00E1rio");
-		lblNewLabel.setBounds(21, 25, 160, 14);
-		contentPane.add(lblNewLabel);
+		JLabel LabelNome = new JLabel("Nome do novo funcionário");
+		LabelNome.setBounds(10, 105, 160, 14);
+		contentPane.add(LabelNome);
+		
+		JLabel lblAdicionarUmNovo = new JLabel("Adicionar um novo funcion\u00E1rio");
+		lblAdicionarUmNovo.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblAdicionarUmNovo.setBounds(28, 17, 401, 102);
+		contentPane.add(lblAdicionarUmNovo);
 		
 		textNome = new JTextField();
-		textNome.setBounds(10, 52, 310, 20);
+		textNome.setBounds(10, 129, 310, 20);
 		contentPane.add(textNome);
 		textNome.setColumns(10);
 		
@@ -68,7 +75,7 @@ public class AdicionarFuncionariosView extends JFrame {
 				janelaFuncionarios.setVisible(true);
 			}
 		});
-		btnVoltar.setBounds(10, 214, 89, 23);
+		btnVoltar.setBounds(171, 424, 89, 23);
 		contentPane.add(btnVoltar);
 		
 		JButton btnNewButton_1 = new JButton("Confirmar");
@@ -77,8 +84,18 @@ public class AdicionarFuncionariosView extends JFrame {
 				funcionariosControlador.adicionarFuncionario();
 			}
 		});
-		btnNewButton_1.setBounds(310, 214, 102, 23);
+		btnNewButton_1.setBounds(270, 424, 102, 23);
 		contentPane.add(btnNewButton_1);
+		
+		JLabel lblCpf = new JLabel("CPF");
+		lblCpf.setBounds(10, 159, 46, 13);
+		contentPane.add(lblCpf);
+		
+		JLabel Fundo = new JLabel("");
+		Fundo.setBounds(0, 455, 237, 20);
+		Fundo.setIcon(new ImageIcon(MenuPrincipalView.class.getResource("/adicionarFuncionario.png")));
+		contentPane.add(Fundo);
+		
 	}
 	
 	public void setJanenelaFuncionarios(FuncionariosView janela) {
@@ -92,5 +109,4 @@ public class AdicionarFuncionariosView extends JFrame {
 	public JTextField getTextNome() {
 		return textNome;
 	}
-	
 }
